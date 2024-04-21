9edffa795b4ff6b21735bb3e565914450edc390c
Kristina <kr.hranovska@gmail.com>
2024-04-21 13:11:34 +0300

    is ready

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,9 +1,13 @@
 const contactsService = require("../services/contactsServices");
 const { HttpError } = require("../helpers/HttpError");
 
-const getAllContacts = async (req, res) => {
-    const result = await contactsService.listContacts();
-    res.json(result);
+const getAllContacts = async (req, res, next) => {
+    try {
+        const result = await contactsService.listContacts();
+        res.json(result);
+    } catch (error) {
+        next(error)
+    }
 };
 
 const getOneContact = async (req, res, next) => {
@@ -20,33 +24,46 @@ const getOneContact = async (req, res, next) => {
     }
 };
 
-const deleteContact = async (req, res) => {
-    const { id } = req.params;
-    const result = await contactsService.removeContact(id);
-    if (!result) {
-        throw HttpError(404, "Not found")
+const deleteContact = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const result = await contactsService.removeContact(id);
+        if (!result) {
+            throw HttpError(404, "Not found")
+        }
+        res.json(result);
+    } catch (error) {
+        next(error)
     }
-    res.json(result);
 };
 
-const createContact = async (req, res) => {
-    const result = await contactsService.addContact(req.body);
-    res.status(201).json(result)
+const createContact = async (req, res, next) => {
+    try {
+        const result = await contactsService.addContact(req.body);
+        res.status(201).json(result)
+    } catch (error) {
+        next(error)
+    }
 };
 
-const updateContact = async (req, res) => {
-    const { id } = req.params;
-    const { data } = req.body
-    const result = await contactsService.updateContact(id, data);
+const updateContact = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const { data } = req.body
+        const result = await contactsService.updateContact(id, data);
 
-    if (!data || Object.keys(data).length === 0) {
-        throw HttpError(400, "Body must have at least one field")
-    }
+        if (!result) {
+            throw HttpError(404, "Not found")
+        }
 
-    if (!result) {
-        throw HttpError(404, "Not found")
+        if (!data || Object.keys(data).length === 0) {
+            throw HttpError(400, "Body must have at least one field")
+        }
+
+        res.json(result)
+    } catch (error) {
+        next(error)
     }
-    res.json(result)
 };
 
 module.exports = {
