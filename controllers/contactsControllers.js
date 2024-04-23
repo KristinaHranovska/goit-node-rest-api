19f577c85ed7da7ef871658e735d9dacc1071fa9
Kristina <kr.hranovska@gmail.com>
2024-04-23 20:28:30 +0300

    mongooseError add

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -5,7 +5,7 @@ const { Contact } = require('../models/contact')
 
 const getAllContacts = async (req, res, next) => {
     try {
-        const result = await Contact.find();
+        const result = await Contact.find({}, "-createdAt, -updatedAt");
         res.json(result);
     } catch (error) {
         next(error)
@@ -26,52 +26,52 @@ const getOneContact = async (req, res, next) => {
     }
 };
 
-// const deleteContact = async (req, res, next) => {
-//     try {
-//         const { id } = req.params;
-//         const result = await contactsService.removeContact(id);
-//         if (!result) {
-//             throw HttpError(404, "Not found")
-//         }
-//         res.json(result);
-//     } catch (error) {
-//         next(error)
-//     }
-// };
+const deleteContact = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const result = await Contact.findByIdAndDelete(id);
+        if (!result) {
+            throw HttpError(404, "Not found")
+        }
+        res.json(result);
+    } catch (error) {
+        next(error)
+    }
+};
 
-// const createContact = async (req, res, next) => {
-//     try {
-//         const result = await contactsService.addContact(req.body);
-//         res.status(201).json(result)
-//     } catch (error) {
-//         next(error)
-//     }
-// };
+const createContact = async (req, res, next) => {
+    try {
+        const result = await Contact.create(req.body);
+        res.status(201).json(result)
+    } catch (error) {
+        next(error)
+    }
+};
 
-// const updateContact = async (req, res, next) => {
-//     try {
-//         const { id } = req.params;
-//         const { data } = req.body
-//         const result = await contactsService.updateContact(id, data);
+const updateContact = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const { data } = req.body
+        const result = await Contact.findByIdAndUpdate(id, data);
 
-//         if (!result) {
-//             throw HttpError(404, "Not found")
-//         }
+        if (!result) {
+            throw HttpError(404, "Not found")
+        }
 
-//         if (!data || Object.keys(data).length === 0) {
-//             throw HttpError(400, "Body must have at least one field")
-//         }
+        if (!data || Object.keys(data).length === 0) {
+            throw HttpError(400, "Body must have at least one field")
+        }
 
-//         res.json(result)
-//     } catch (error) {
-//         next(error)
-//     }
-// };
+        res.json(result)
+    } catch (error) {
+        next(error)
+    }
+};
 
 module.exports = {
     getAllContacts,
     getOneContact,
-    // deleteContact,
-    // createContact,
-    // updateContact
+    deleteContact,
+    createContact,
+    updateContact
 }
--- models/contact.js
+++ models/contact.js
@@ -1,6 +1,8 @@
 const { Schema, model } = require('mongoose');
 const Joi = require("joi");
 
+const { mongooseError } = require("../helpers/mongooseError")
+
 const phoneRegExp = /^\(\d{3}\) \d{3}-\d{4}$/;
 
 const contactSchema = new Schema({
@@ -10,27 +12,33 @@ const contactSchema = new Schema({
     },
     email: {
         type: String,
+        required: true,
     },
     phone: {
         type: String,
         match: phoneRegExp,
+        required: true,
     },
     favorite: {
         type: Boolean,
         default: false,
     },
-})
+}, { versionKey: false, timestamps: true });
+
+contactSchema.post('save', mongooseError)
 
 const createContactSchema = Joi.object({
     name: Joi.string().required(),
     email: Joi.string().required(),
     phone: Joi.string().required(),
+    favorite: Joi.boolean(),
 })
 
 const updateContactSchema = Joi.object({
     name: Joi.string(),
     email: Joi.string(),
     phone: Joi.string(),
+    favorite: Joi.boolean(),
 })
 
 const schemas = {
