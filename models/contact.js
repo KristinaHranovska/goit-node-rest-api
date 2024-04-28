907b48edae8d96e089a36d795c61db749cb3d8e3
Kristina <kr.hranovska@gmail.com>
2024-04-28 21:45:24 +0300

    schema is ready

--- models/contact.js
+++ models/contact.js
@@ -1,7 +1,7 @@
 const { Schema, model } = require('mongoose');
 const Joi = require("joi");
 
-const { mongooseError } = require("../helpers/mongooseError")
+const { mongooseError } = require("../helpers/mongooseError");
 
 const phoneRegExp = /^\(\d{3}\) \d{3}-\d{4}$/;
 
@@ -25,21 +25,21 @@ const contactSchema = new Schema({
     },
 }, { versionKey: false, timestamps: true });
 
-contactSchema.post('save', mongooseError)
+contactSchema.post('save', mongooseError);
 
 const createContactSchema = Joi.object({
     name: Joi.string().required(),
     email: Joi.string().required(),
     phone: Joi.string().required(),
     favorite: Joi.boolean(),
-})
+});
 
 const updateContactSchema = Joi.object({
     name: Joi.string(),
     email: Joi.string(),
     phone: Joi.string(),
     favorite: Joi.boolean(),
-})
+});
 
 const schemas = {
     createContactSchema,
