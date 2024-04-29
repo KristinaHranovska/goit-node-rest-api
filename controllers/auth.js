9912629e9b38240f81e273fe36e2497d0c37668c
Kristina <kr.hranovska@gmail.com>
2024-04-29 09:34:28 +0300

    add next

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,25 @@
 const { User } = require('../models/user');
 const { HttpError } = require("../helpers/HttpError");
 
-const registration = async (req, res) => {
-    const newUser = await User.create(req.body);
+const registration = async (req, res, next) => {
+    // const { email, password } = req.body;
+    try {
+        // const user = await User.findOne({ email });
 
-    res.json({
-        email: newUser.email,
-        name: newUser.name,
-    })
+        // if (user) {
+        //     throw HttpError(409, "Email already in use");
+        // }
+
+        const newUser = await User.create(req.body);
+
+        res.status(201).json({
+            email: newUser.email,
+            name: newUser.name,
+        })
+    }
+    catch (error) {
+        next(error)
+    }
 }
 
 module.exports = {
